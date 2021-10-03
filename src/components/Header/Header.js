47c7a015a5b722f6535987d9443c08367d937cf4
krishnaCheashty <krishnacheashty@gmail.com>
2021-10-04 02:07:39 +0600

    ok

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>i am header section</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            hello
+        </div>
+    );
+};
+
+export default Home;
